e76349452a4eb2dec41e4bd228baa7c8ad0baa9e
Levi Crider <levicrider08@gmail.com>
2023-02-01 13:18:47 -0600

    Changes

--- HW1/Geometry.py
+++ HW1/Geometry.py
@@ -1,10 +1,12 @@
 import numpy as np
 class Geometry:
+    numInstances = 0;
     def __init__(self, name = "Shape", points = None):
         self.name = name
         # name is string that is a name of geometry
         self.points = points
         # points are a list of tuple points = [(x0, y0), (x1, y1), ...]
+        Geometry.numInstances += 1;
         def calculate_area(self):
             pass
 
@@ -13,8 +15,7 @@ class Geometry:
 
         @classmethod
         def count_number_of_geometry(cls):
-            pass;
-
+            return Geometry.numInstances;
 class Triangle(Geometry):
     def __init__(self, a, b, c):
         super(Triangle, self).__init__("Triangle",[a,b,c]);
@@ -28,18 +29,47 @@ class Rectangle(Geometry):
         super(Rectangle, self).__init__("Rectangle",[a,b]);
 
     def calculate_area(self):
-        x1, y1 = self.points[0][0];
-        x2, y2 = self.points[0][1];
-        x3,y3 = self.points[1][0];
-        x4,y4 = self.points[1][1];
+        x1,y1 = self.points[0];
+        x2,y2 = self.points[1];
+        length = (x2-x1);
+        height = (y2-y1);
+        area = length * height;
+        return area;
+class Square(Rectangle):
+    def __init__(self, a, length):
+        super(Square, self).__init__(a=a, b=None);
+        self.length = length;
 
-        length = np.sqrt((x2 - x1)**2 + (y2 - y1)**2);
-        width = np.sqrt((x4 - x1)**2 +  (y4 - y1)**2);
+    def calculate_area(self):
+        area = self.length ** 2;
+        return area;
+        
+class Circle(Geometry):
+    def __init__(self, o, r):
+        super(Circle, self).__init__("Circle",o);
+        self.radius = r;
 
-        area = length * width;
-        print(area);
+    def calculate_area(self):
+        area = np.pi * (self.radius ** 2);
+        return area;
+class Polygon(Geometry):
+    def __init__(self, points):
+        super(Polygon, self).__init__("Polygon",points=points);
 
-x = Geometry();
+    def calculate_area(self):
+        pass;
 
-y = Rectangle([(0,0), (0,1) ], [(2,1), (2,0)]);
-y.calculate_area();
+def main():
+    a = Square((1,1), 10);
+    a.calculate_area();
+    b = Circle((1,1), 9);
+    b.calculcate_area();
+    c = Geometry();
+    print(c.numInstances);
+    d = Polygon([(1,0), (1,1), (0,2), (-2,1), (-1,0)])
+    e = Rectangle((0, 0), (2, 2))
+
+if __name__ == "__main__":
+    main();
+
+
---
+++ HW1/Test.py
@@ -0,0 +1,70 @@
+import random
+import numpy as np
+from Geometry import Triangle, Rectangle, Square, Circle, Polygon
+
+##############################
+## Test cases for Problem 1 ##
+##############################
+
+# triangle = Triangle((0, 1), (1, 0), (0, 0))
+# print("Area of %s: %0.4f" % (triangle.name, triangle.calculate_area()))
+
+rectangle = Rectangle((0, 0), (2, 2))
+print("Area of %s: %0.4f" % (rectangle.name, rectangle.calculate_area()))
+
+square = Square((0, 0), 2)
+print("Area of %s: %0.4f" % (square.name, square.calculate_area()))
+
+circle = Circle((0, 0), 3)
+print("Area of %s: %0.4f" % (circle.name, circle.calculate_area()))
+
+# polygon = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
+# print("Area of %s: %0.4f" % (polygon.name, polygon.calculate_area()))
+
+#########################################
+## Test cases for matrix multplication ##
+#########################################
+
+# for test in range(10):
+#     m, n, k = random.randint(3, 10), random.randint(3, 10), random.randint(3, 10)
+#     A = np.random.randn(m, n)
+#     B = np.random.randn(n, k)
+#     assert np.mean(np.abs(A.dot(B) - matrix_multilication(A, B))) <= 1e-7, "Your implmentation is wrong!"
+#     print("[Test Case %d]. Your implementation is correct!" % test)
+
+#####################################
+## Test cases for the pow function ##
+#####################################
+
+# for test in range(10):
+#     n = random.randint(2, 5)
+#     A = np.random.randn(n, n)
+#     print("A^{} = {}".format(n, pow(A, n)))
+
+
+#####################################################
+## Test Cases for Fibonacci and Recursive Sequence ##
+#####################################################
+
+# a, b = 1, 1
+# for i in range(2, 10):
+#     c = a + b
+#     assert (fibo(i) == c), "You implementation is incorrect"
+#     print("[Test Case %d]. Your implementation is correct!. fibo(%d) = %d" % (i - 2, i, fibo(i)))
+#     a = b
+#     b = c
+
+# for n in range(5, 11):
+#     for k in range(2, 5):
+#         print("f(%d, %d) = %d" % (n, k, f(n, k)))
+
+########################################### 
+## Test Cases for BFS, DFS, Find Minimum ##
+###########################################
+
+# As = np.load("test.npy", allow_pickle=True)
+# for test in range(len(As)):
+#     A = As[test]
+#     print("Test Case %d" % test)
+#     print(A) 
+#     BFS(A) # or DFS(A) or FindMinium(A) 
