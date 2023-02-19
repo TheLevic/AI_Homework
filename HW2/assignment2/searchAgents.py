98adf451f0faa92aa3023f0bcf9c521ff24010df
ethanweems <emweems@uark.edu>
2023-02-19 13:37:58 -0600

    added comment line 14

--- HW2/assignment2/searchAgents.py
+++ HW2/assignment2/searchAgents.py
@@ -11,6 +11,7 @@
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
+# what up dog, it's ur boi boogie2988
 
 """
 This file contains all of the agents that can be selected to control Pacman.  To
